15978708537b8136040a42a454099defbd4ba266
ulgoon <ulgoon89@gmail.com>
2017-03-29 15:18:54 +0900

    add if condition

--- 2nd/sources/numguess.py
+++ 2nd/sources/numguess.py
@@ -6,4 +6,8 @@ print(answer)
 
 username = input("What is your name? ")
 guess = eval(input("Hi, " + username + ". Guess the number: "))
-print(guess)
+
+if guess == answer:
+    print("Correct!")
+else:
+    print("You are wrong!")
