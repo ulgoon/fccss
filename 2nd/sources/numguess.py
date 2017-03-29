41f6b739107943f7e9a92859da5c28b2b88f7a51
ulgoon <ulgoon89@gmail.com>
2017-03-29 16:10:33 +0900

    add while in judge

--- 2nd/sources/numguess.py
+++ 2nd/sources/numguess.py
@@ -5,9 +5,12 @@ answer = random.randint(1,100)
 print(answer)
 
 username = input("What is your name? ")
-guess = eval(input("Hi, " + username + ". Guess the number: "))
 
-if guess == answer:
-    print("Correct!")
-else:
-    print("You are wrong!")
+while True:
+    guess = eval(input("Hi, " + username + ". Guess the number: "))
+
+    if guess == answer:
+        print("Correct!")
+        break
+    else:
+        print("You are wrong!")
