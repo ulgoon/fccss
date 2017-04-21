a460cb19bc507176d7895aa7094bb4a0c1207bcf
ulgoon <ulgoon89@gmail.com>
2017-04-21 16:55:20 +0900

    add sock_data_client.py

---
+++ 2nd/sources/sock_data_client.py
@@ -0,0 +1,24 @@
+import socket
+
+
+host = 'www.google.com'
+port = 80
+bufsiz = 4096
+addr = (host, port)
+
+if __name__ == '__main__':
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock.connect(addr)
+
+    while True:
+        data = 'GET / HTTP/1.0\r\n\r\n'
+        if not data:
+            break
+        client_sock.send(data.encode('utf-8'))
+        data = client_sock.recv(bufsiz)
+
+        if not data:
+            break
+        print(data.decode('utf-8'))
+
+    client_sock.close()
