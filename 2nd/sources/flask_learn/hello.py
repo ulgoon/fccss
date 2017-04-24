ae89f0d35d2a99f730b46e8295df61e2e644a646
ulgoon <ulgoon89@gmail.com>
2017-04-24 15:55:03 +0900

    add route about

--- 2nd/sources/flask_learn/hello.py
+++ 2nd/sources/flask_learn/hello.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return "First Flask Application!!!"
 
+@app.route('/about')
+def about():
+    return "This is about"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
