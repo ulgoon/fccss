e31f493aa54611289aedc5f0bee6ba24c310b731
ulgoon <ulgoon89@gmail.com>
2017-04-24 15:52:45 +0900

    flask init

---
+++ 2nd/sources/flask_learn/hello.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "First Flask Application!!!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
