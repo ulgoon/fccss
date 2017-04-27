fe1c2edcdfae281c899276f8429173a806cfb8d3
ulgoon <ulgoon89@gmail.com>
2017-04-27 13:44:51 +0900

    add post method

--- 2nd/sources/flask_fakebook/app.py
+++ 2nd/sources/flask_fakebook/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3 as lite
 
 
 app = Flask(__name__)
@@ -6,9 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
+
+@app.route('/postuser', methods = ['POST', 'GET'])
+def postuser():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            age = request.form['age']
+            email = request.form['email']
+            
+            with lite.connect("users.db") as conn:
+                cur = conn.cursor()
+                
+                cur.execute("INSERT INTO user (name, age, email) VALUES(?, ?, ?)", (name, age, email))
+                conn.commit()
+                msg = "Signup complete"
+
+        except:
+            conn.rollback()
+            msg = "Signup Failed"
+        finally:
+            return render_template("signup.html", msg = msg)
+            conn.close()
+
 
 @app.route('/users')
 def users():
