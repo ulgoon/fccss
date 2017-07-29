0ef4712d78aacafc35f1fe3cf09d5e718a802fa9
ulgoon <ulgoon89@gmail.com>
2017-07-29 14:23:00 +0900

    edit is_leap.py return string with year

--- 3rd/sources/week1/is_leap.py
+++ 3rd/sources/week1/is_leap.py
@@ -1,8 +1,10 @@
-leap = False
+# leap = False
 def is_leap(year):
     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        leap = True
-        return leap
+        # leap = True
+        return "%5d is leap year" % (year)
+    else:
+        return "%5d is not leap year" % (year)
 
 year = int(input("Is leap? "))
 print(is_leap(year))
