312cc9111b0a55c075a54d1d8553342311d2f8db
ulgoon <ulgoon89@gmail.com>
2017-07-29 14:07:29 +0900

    edit fizzbuzz.py without condition get rid of if, else

--- 3rd/sources/week1/fizzbuzz.py
+++ 3rd/sources/week1/fizzbuzz.py
@@ -1,8 +1,5 @@
 num = int(input("type the number: "))
 
 for i in range(1,num+1):
-    if i % 3 == 0 or i % 5 == 0:
-        print("fizz"*(not i%3) + "buzz"*(not i%5))
-    else:
-        print(i)
+        print("fizz"*(not i%3) + "buzz"*(not i%5) or i)
 
