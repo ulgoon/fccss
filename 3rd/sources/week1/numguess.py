078b0803027b30a31a74da841e597d8322d3c8dd
ulgoon <ulgoon89@gmail.com>
2017-07-29 13:42:45 +0900

    add repeat loop

--- 3rd/sources/week1/numguess.py
+++ 3rd/sources/week1/numguess.py
@@ -8,10 +8,14 @@ print(answer)
 
 # get username, 1 integer for guessing and print username
 username = input("Hi, What is your name? ")
-guess = eval(input("Hi, %s. Guess the number(1 to 100) - " % (username)))
 
-# main logic to compare with guess(user input) and answer
-if guess == answer:
-    print("Correct!")
-else:
-    print("Wrong!")
+# repeat ask and print loop
+while True:
+    guess = eval(input("Hi, %s. Guess the number(1 to 100) - " % (username)))
+
+    # main logic to compare with guess(user input) and answer
+    if guess == answer:
+        print("Correct!")
+        break
+    else:
+        print("Wrong!")
