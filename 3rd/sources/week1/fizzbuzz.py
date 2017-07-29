8b7d5c7519c57703244867eca7dbbc957ba7df92
ulgoon <ulgoon89@gmail.com>
2017-07-29 14:06:25 +0900

    edit fizzbuzz.py get rid of elif

--- 3rd/sources/week1/fizzbuzz.py
+++ 3rd/sources/week1/fizzbuzz.py
@@ -1,12 +1,8 @@
 num = int(input("type the number: "))
 
 for i in range(1,num+1):
-    if i % 15 == 0:
-        print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
+    if i % 3 == 0 or i % 5 == 0:
+        print("fizz"*(not i%3) + "buzz"*(not i%5))
     else:
         print(i)
 
