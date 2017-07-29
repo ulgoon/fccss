8face804e7fa569e30285410d53efb44d753561b
ulgoon <ulgoon89@gmail.com>
2017-07-29 14:09:03 +0900

    fizzbuzz with list comprehension

--- 3rd/sources/week1/fizzbuzz.py
+++ 3rd/sources/week1/fizzbuzz.py
@@ -1,5 +1,5 @@
 num = int(input("type the number: "))
 
-for i in range(1,num+1):
-        print("fizz"*(not i%3) + "buzz"*(not i%5) or i)
+fizzbuzz_list = ["fizz"*(not i%3) + "buzz"*(not i%5) or i for i in range(1,num+1)]
+print(fizzbuzz_list)
 
