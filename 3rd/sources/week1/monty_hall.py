e7d8d21c9eeb4eef47ed00a448c10299c279dda6
ulgoon <ulgoon89@gmail.com>
2017-07-30 20:58:59 +0900

    add commentary on monty_hall simulation

--- 3rd/sources/week1/monty_hall.py
+++ 3rd/sources/week1/monty_hall.py
@@ -1,9 +1,14 @@
+# import random library
 import random
 
-
+# initialize stay, switch
 stay = 0
 switch = 0
 
+# set doors 1 car(1), 2 goats(0)
+# get user's choice with randrange
+# if user's choice was 1, stay + 1
+# if not, switch + 1
 for i in range(1000):
     doors = [1, 0, 0]
     random.shuffle(doors)
@@ -17,5 +22,6 @@ for i in range(1000):
     else:
         switch = switch + 1
 
+# after 1000 times of simulation, print the results
 print("stay: %4d" % stay)
 print("switch: %4d" % switch)
