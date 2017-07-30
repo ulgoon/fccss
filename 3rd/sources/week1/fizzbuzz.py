b0dca773eac36c1b5d4566b4a698851d75d50987
ulgoon <ulgoon89@gmail.com>
2017-07-30 20:50:02 +0900

    add commentary for legacy

--- 3rd/sources/week1/fizzbuzz.py
+++ 3rd/sources/week1/fizzbuzz.py
@@ -3,3 +3,25 @@ num = int(input("type the number: "))
 fizzbuzz_list = ["fizz"*(not i%3) + "buzz"*(not i%5) or i for i in range(1,num+1)]
 print(fizzbuzz_list)
 
+# first fizzbuzz
+# num = int(input("type the number: "))
+#
+# for i in range(1, num+1):
+#    if i % 15 == 0:
+#        print("fizzbuzz")
+#    elif i % 3 == 0:
+#        print("fizz")
+#    elif i % 5 == 0:
+#        print("buzz")
+#    else:
+#        print(i)
+
+# edit fizzbuzz conditions
+# if i % 3 == 0 or i % 5 == 0:
+#     print("fizz"*(not i%3) + "buzz"*(not i%5))
+# else:
+#     print(i)
+
+# edit fizzbuzz without conditions
+# print("fizz"*(not i%3) + "buzz"*(not i%5) or i)
+
