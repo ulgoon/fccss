213f1f41077768ccccd509fd04aaa2db20e6b11d
ulgoon <ulgoon89@gmail.com>
2017-08-23 16:29:43 +0900

    edit hello.py for render templates(html files)

--- 3rd/sources/week5/simple_webserver/hello.py
+++ 3rd/sources/week5/simple_webserver/hello.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello Flask!!!"
+def index(name=None):
+    return render_template("index.html", name=name)
 
 @app.route('/about')
-def about():
-    return "This is about!!"
+def about(name=None):
+    return render_template("about.html", name=name)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0') #127.0.0.1
