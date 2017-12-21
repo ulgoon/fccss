3cd7b128230948650f66b0025d91f415346b8c4e
ulgoon <ulgoon89@gmail.com>
2017-12-21 16:38:48 +0900

    add fakebook

--- 4th/resources/fakebook/server.py
+++ 4th/resources/fakebook/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3 as lite
 
 
 app = Flask(__name__)
@@ -12,10 +13,38 @@ def index():
 def signup():
     return render_template("signup.html")
 
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            age = request.form['age']
+            lang = request.form['lang']
+
+            with lite.connect('users.db') as conn:
+                cur = conn.cursor()
+                cur.execute('INSERT INTO user(name, age, lang) VALUES(?,?,?);', (name, age, lang))
+                conn.commit()
+
+                msg = "Sign up Success"
+        except:
+            conn.rollback()
+            msg = "Sign up Failed"
+        finally:
+            return render_template("signup.html", msg = msg)
+
 # users => 모든 사용자를 조회할 수 있는 페이지
 @app.route('/users')
 def users():
-    return render_template("users.html")
+    conn = lite.connect('users.db')
+    conn.row_factory = lite.Row
+
+    cur = conn.cursor()
+    cur.execute("SELECT * from user;")
+
+    rows = cur.fetchall()
+    conn.close()
+    return render_template("users.html", rows = rows)
 
 
 @app.route('/hello')
